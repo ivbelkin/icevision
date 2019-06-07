91c0efb67b5b0aaa4481360f07ca63403871413a
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2019-06-07 20:44:57 +0300

    add bbox filtering

--- icevision/predictions_to_cvat.py
+++ icevision/predictions_to_cvat.py
@@ -1,9 +1,8 @@
 import argparse
-import os
-import pandas as pd
 import torch
 
 from tqdm import tqdm
+from maskrcnn_benchmark.layers import nms as box_nms
 
 from icevision.cvat import CvatDataset
 
@@ -19,6 +18,16 @@ def build_parser():
         "--output-file",
         help="file with *.xml cvat annotations"
     )
+    parser.add_argument(
+        "--conf-threshold",
+        type=float,
+        default=0.1
+    )
+    parser.add_argument(
+        "--nms-threshold",
+        type=float,
+        default=0.3
+    )
     return parser
 
 
@@ -28,9 +37,19 @@ def main(args):
 
     for image_id, prediction in enumerate(tqdm(predictions)):
         ds.add_image(image_id)
-        for box_id in range(len(prediction)):
+        boxes = prediction.bbox
+        scores = prediction.get_field("scores")
+        if args.nms_threshold > 0:
+            keep_ids = box_nms(boxes, scores, args.nms_threshold)
+        else:
+            keep_ids = range(len(prediction))
+
+        for box_id in keep_ids:
             bbox = prediction.bbox[box_id].numpy()
             label = prediction.get_field("labels")[box_id].numpy()
+            score = prediction.get_field("scores")[box_id].numpy()
+            if score < args.conf_threshold:
+                continue
             ds.add_box(
                 image_id, xtl=bbox[0], ytl=bbox[1], xbr=bbox[2], ybr=bbox[3], label=label
             )
